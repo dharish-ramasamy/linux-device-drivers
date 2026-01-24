ee77b0f70f295dd6ef3299b1e8a3f5732d0e2cd8
dharish-ramasamy <dharishjayakodi@gmail.com>
2026-01-24 13:29:21 +0000

    process_info module added

---
+++ module_programming/Q5/process_info.c
@@ -0,0 +1,30 @@
+#include<linux/module.h>
+#include<linux/init.h>
+#include<linux/kernel.h>
+#include<linux/sched.h>
+#include<linux/timekeeping.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Dharish");
+MODULE_DESCRIPTION("Process info module");
+
+static int __init insert_process_info(void)
+{
+   struct task_struct *task = current;
+   printk(KERN_INFO "Process info module inserted..\n");
+   printk(KERN_INFO "PID: %d\n", task -> pid);
+   printk(KERN_INFO "PPID: %d\n", task -> parent -> pid);
+   printk(KERN_INFO "TGID: %d\n", task -> tgid);
+   printk(KERN_INFO "COMMAND: %s\n", task -> comm);
+   printk(KERN_INFO "PRIOITY: %d\n", task -> prio);
+   return 0;
+}
+
+static void __exit remove_process_info(void)
+{
+   printk(KERN_INFO "Process info module removed...\n");
+}
+
+module_init(insert_process_info);
+module_exit(remove_process_info);
+
